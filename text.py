4367f5689931c1d22f468f296081a7be3036e6e6
subhojit <subhojitrockstar@gmail.com>
2023-07-09 01:58:10 +0530

    python

---
+++ text.py
@@ -0,0 +1,50 @@
+# test='test'
+# print(test)
+# print(type(test))
+#
+# print(f"This is {test}")
+# print(f"This is {10+10}")
+# l1 =[1,'Sham']
+# t1 =(1,'Sham')
+# l1[0]="Sita"
+#
+# print(l1)
+# print(t1)
+# a=input("Type a number:")
+# print(a)
+# thislist = ["apple", "banana", "cherry"]
+# thislist.insert(1, "orange")
+# print(thislist)
+# thislist = ["apple", "banana", "cherry"]
+# thislist.append("orange")
+# print(thislist)
+# thislist = ["apple", "banana", "cherry"]
+# tropical = ["mango", "pineapple", "papaya"]
+# thislist.extend(tropical)
+# print(thislist)
+# thislist = ["apple", "banana", "cherry"]
+# thislist.remove("banana")
+# print(thislist)
+# thislist = ["apple", "banana", "cherry"]
+# thislist.pop(1)
+# print(thislist)
+# thislist = ["apple", "banana", "cherry"]
+# del thislist[0]
+# print(thislist)
+# thislist = ["apple", "banana", "cherry"]
+# thislist.clear()
+# print(thislist)
+#
+# thislist = ["apple", "banana", "cherry"]
+# for x in thislist:
+#     print(x)
+# thislist = ["apple", "banana", "cherry"]
+# for i in range(len(thislist)):
+#  print(i)
+thislist = ["apple", "banana", "cherry"]
+x=0
+while x<(len(thislist)):
+    print(thislist[x])
+    x = x + 1
+
+
