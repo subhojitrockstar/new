bb7a5e5a995786a1a1318693732568a9d9f1fe3e
subhojit <subhojitrockstar@gmail.com>
2023-07-10 21:53:42 +0530

    Python_comments

--- python_exam.py
+++ python_exam.py
@@ -3,3 +3,13 @@
 print("Hello World")
 if 5 > 2:
  print("Five is greater than two!")
+#  Exercise:
+# This example misses indentations to be correct.
+#
+# Insert the missing indentation to make the code correct:
+
+
+# This is a comment
+'''This is a comment
+written in 
+more than just one line'''
