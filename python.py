0bfdb155b20d56e358f23b6487bd81bc941c2358
subhojit <subhojitrockstar@gmail.com>
2023-07-14 10:22:46 +0530

    Learning

---
+++ python.py
@@ -0,0 +1,84 @@
+# test='test'
+# print(test)
+# print(type(test))
+#
+# print(f"This is {test}")
+# print(f"This is {10+10}")
+# l1 =[1,'Sham']
+# t1 =(1,'Sham')
+# l1[0]="Sita"
+#
+# print(l1)
+# print(t1)
+# a=input("Type a number:")
+# print(a)
+# thislist = ["apple", "banana", "cherry"]
+# thislist.insert(1, "orange")
+# print(thislist)
+# thislist = ["apple", "banana", "cherry"]
+# thislist.append("orange")
+# print(thislist)
+# thislist = ["apple", "banana", "cherry"]
+# tropical = ["mango", "pineapple", "papaya"]
+# thislist.extend(tropical)
+# print(thislist)
+# thislist = ["apple", "banana", "cherry"]
+# thislist.remove("banana")
+# print(thislist)
+# thislist = ["apple", "banana", "cherry"]
+# thislist.pop(1)
+# print(thislist)
+# thislist = ["apple", "banana", "cherry"]
+# del thislist[0]
+# print(thislist)
+# thislist = ["apple", "banana", "cherry"]
+# thislist.clear()
+# print(thislist)
+#
+# thislist = ["apple", "banana", "cherry"]
+# for x in thislist:
+#     print(x)
+# thislist = ["apple", "banana", "cherry"]
+# for i in range(len(thislist)):
+#  print(i)
+# thislist = ["apple", "banana", "cherry"]
+# x=0
+# while x<(len(thislist)):
+#     print(thislist[x])
+#     x = x + 1
+# test = [1,4]
+# test1 = [j*3 for j in test]
+# print(test1)
+# thislist = ["b", "m", "d", "a", "c"]
+# thislist.sort()
+# print(thislist)
+#
+# thislist = [1,3,8,9]
+# thislist.sort()
+# print(thislist)
+#
+# thislist = [1,3,4,5]
+# thislist.sort(reverse=True)
+# print(thislist)
+# thislist.copy()
+# print(thislist)
+# list1 = ["a", "b" , "c"]
+# list2 = [1, 2, 3]
+#
+# list1.extend(list2)
+# print(list1)
+
+# Python - List Methods
+
+# append()	Adds an element at the end of the list
+# clear()	Removes all the elements from the list
+# copy()	Returns a copy of the list
+# count()	Returns the number of elements with the specified value
+# extend()	Add the elements of a list (or any iterable), to the end of the current list
+# index()	Returns the index of the first element with the specified value
+# insert()	Adds an element at the specified position
+# pop()	Removes the element at the specified position
+# remove()	Removes the item with the specified value
+# reverse()	Reverses the order of the list
+# sort()	Sorts the list
+
