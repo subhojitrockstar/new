a99aa101edcfb60f2192290720b28e4e35ec6026
subhojit <subhojitrockstar@gmail.com>
2023-07-27 06:39:47 +0530

    Learning

--- python.py
+++ python.py
@@ -429,3 +429,15 @@
 # print(x)
 # print(x.strftime("%A"))
 #Math
+# class Person:
+#   def __init__(self, fname):
+#     self.firstname = fname
+#
+#   def printname(self):
+#     print(self.firstname)
+#
+# class Student(Person):
+#   pass
+#
+# x = Student("Mike")
+# x.printname()
