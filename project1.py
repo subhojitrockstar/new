afddc1c389f4436b3ffe4b7eea145b4f6ad93561
subhojit <subhojitrockstar@gmail.com>
2023-08-04 07:01:30 +0530

    Learning

--- project1.py
+++ project1.py
@@ -4,8 +4,8 @@ ALL = ["1.pdf", "2.pdf"]
 Pdf_merge = PdfMerger()
 
 for newpdf in Pdf_merge:
-    merge.append(newpdf)
+    Pdf_merge.append(newpdf)
 
-merge.write("allnew.pdf")
-merge.close()
+Pdf_merge.write("allnew.pdf")
+Pdf_merge.close()
 
